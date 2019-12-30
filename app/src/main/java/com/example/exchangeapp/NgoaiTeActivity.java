acb3eaf340d948b9d3c5bec779e7c6d93895eed0
longnct7090 <longnct7090@co-well.com.vn>
2019-12-30 16:43:33 +0700

    update 30122019v1

--- app/src/main/java/com/example/exchangeapp/NgoaiTeActivity.java
+++ app/src/main/java/com/example/exchangeapp/NgoaiTeActivity.java
@@ -18,9 +18,19 @@ public class NgoaiTeActivity extends AppCompatActivity {
     private Button btnTienAo;
 
     private Button btnUSD, btnEUR, btnJPY, btnKRW, btnCNY, btnGDP, btnSGD, btnCAD, btnTHB, btnRUB;
+
     private TextView txtMuaTienMat1, txtMuaTienMat2, txtMuaTienMat3, txtMuaTienMat4, txtMuaTienMat5,
         txtMuaTienMat6, txtMuaTienMat7, txtMuaTienMat8, txtMuaTienMat9, txtMuaTienMat10, txtMuaTienMat11;
-    private TextView txtMuaChuyenKhoan1;
+
+    private TextView txtMuaChuyenKhoan1, txtMuaChuyenKhoan2, txtMuaChuyenKhoan3, txtMuaChuyenKhoan4, txtMuaChuyenKhoan5,
+        txtMuaChuyenKhoan6, txtMuaChuyenKhoan7, txtMuaChuyenKhoan8, txtMuaChuyenKhoan9, txtMuaChuyenKhoan10, txtMuaChuyenKhoan11;
+
+    private TextView txtBanTienMat1, txtBanTienMat2, txtBanTienMat3, txtBanTienMat4, txtBanTienMat5, txtBanTienMat6,
+    txtBanTienMat7, txtBanTienMat8, txtBanTienMat9, txtBanTienMat10, txtBanTienMat11;
+
+    private TextView txtBanChuyenKhoan1, txtBanChuyenKhoan2, txtBanChuyenKhoan3, txtBanChuyenKhoan4, txtBanChuyenKhoan5,
+            txtBanChuyenKhoan6, txtBanChuyenKhoan7, txtBanChuyenKhoan8, txtBanChuyenKhoan9, txtBanChuyenKhoan10, txtBanChuyenKhoan11;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,12 +74,24 @@ public class NgoaiTeActivity extends AppCompatActivity {
 
         /**
          * Call API this here
+         * Param string link
+         * return JSON data
          */
         CallAPi callAPI = new CallAPi();
 
         /**
          * init button currency
+         * Set event button here
          */
-
+        btnUSD = (Button) findViewById(R.id.btnUSD);
+        btnEUR = (Button) findViewById(R.id.btnEUR);
+        btnJPY = (Button) findViewById(R.id.btnJPY);
+        btnKRW = (Button) findViewById(R.id.btnKRW);
+        btnCNY = (Button) findViewById(R.id.btnCNY);
+        btnGDP = (Button) findViewById(R.id.btnGDP);
+        btnSGD = (Button) findViewById(R.id.btnSGD);
+        btnCAD = (Button) findViewById(R.id.btnCAD);
+        btnTHB = (Button) findViewById(R.id.btnTHB);
+        btnRUB = (Button) findViewById(R.id.btnRUB);
     }
 }
